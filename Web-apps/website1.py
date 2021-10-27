bb324d33924d256e71f688d70609ed98f83b84a7
RishiKumarRay <rishi.ray@knoldus.com>
2021-10-27 11:57:00 +0530

    First Commit

--- Web-apps/website1.py
+++ Web-apps/website1.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello server1'
+    return 'Hello From server1'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
--- Web-apps/website2.py
+++ Web-apps/website2.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello server2'
+    return 'Hello From server2'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5002)
